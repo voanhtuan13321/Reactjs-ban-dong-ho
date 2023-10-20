3555dd4f15b3da4b4641c0b026fb6201e5ef1171
AnhTuan <voanhtuan13321@gmail.com>
2023-10-20 21:31:18 +0700

    add color to tailwingd config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-red': '#eb7260',
+        'main-black': '#354458',
+      },
+    },
   },
   plugins: [],
 };
