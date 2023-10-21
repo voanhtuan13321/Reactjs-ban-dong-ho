1f49ba733da6217f4eff3eed294f7006d9bc09a6
AnhTuan <voanhtuan13321@gmail.com>
2023-10-21 08:50:36 +0700

    modify header

--- src/components/client/Header.jsx
+++ src/components/client/Header.jsx
@@ -10,26 +10,29 @@ export default function Header() {
           <Navbar />
         </div>
         <div>
-          {/* <div>
-            <Link
-              to='/login'
-              className='ml-3 bg-white py-1 px-2 rounded-md'
-            >
-              Login
-            </Link>
-            <Link
-              to='/register'
-              className='ml-3 bg-white py-1 px-2 rounded-md'
-            >
-              Register
-            </Link>
-          </div> */}
-          <div className='w-10 bg-white rounded-full border-2 border-white'>
-            <img
-              src='/profile.svg'
-              alt='profile'
-            />
-          </div>
+          {true ? (
+            <div className='w-10 bg-white rounded-full border-2 border-white'>
+              <img
+                src='/profile.svg'
+                alt='profile'
+              />
+            </div>
+          ) : (
+            <div>
+              <Link
+                to='/login'
+                className='ml-3 bg-white py-1 px-2 rounded-md'
+              >
+                Login
+              </Link>
+              <Link
+                to='/register'
+                className='ml-3 bg-white py-1 px-2 rounded-md'
+              >
+                Register
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </header>
