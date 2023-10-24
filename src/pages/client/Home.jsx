629962196cdeedb07cf50178db5ca3169fa2859a
AnhTuan <voanhtuan13321@gmail.com>
2023-10-24 20:25:06 +0700

    fix

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Carousel from '../../components/client/Carousel';
 import Brands from '../../components/client/Brands';
 import Top5Product from '../../components/client/Top5Product';
 import Search from '../../components/client/Search';
 import FilterMoney from '../../components/client/FilterMoney';
 import ListProduct from '../../components/client/ListProduct';
+import requestHandle from '../../utils/requestHandle';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await requestHandle.get('product/');
+      const data = await response.data;
+      setProducts(data);
+    } catch (err) {
+      console.log(err);
+      navigate('/error');
+    }
+  };
+
   return (
     <div className='w-container m-auto mt-2'>
       <Carousel />
@@ -17,7 +37,7 @@ export default function Home() {
         <FilterMoney />
       </div>
       <div className='relative z-10'>
-        <ListProduct />
+        <ListProduct products={products} />
       </div>
     </div>
   );
