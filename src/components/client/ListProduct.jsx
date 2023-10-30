78d9e3459e4d08d25fea8dbba6ec12989a6a476b
manhle117 <leducmanh113@gmail.com>
2023-10-30 20:44:22 +0700

    mmm

--- src/components/client/ListProduct.jsx
+++ src/components/client/ListProduct.jsx
@@ -1,52 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardItem from './CardItem';
 import { GrNext, GrPrevious } from 'react-icons/gr';
 import ReactPaginate from 'react-paginate';
 
-function ListProduct({ data }) {
-  
+function ListProduct({ products,filteredList }) {
   const [currentPage, setCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   const itemsPerPage = 8;
+
+  useEffect(() => {
+    setPageCount(Math.ceil(filteredList.length / itemsPerPage));
+    setCurrentPage(0);
+  }, [filteredList]);
+
   const handlePageChange = ({ selected }) => {
     setCurrentPage(selected);
   };
+
   const offset = currentPage * itemsPerPage;
-  const currentPageData = products.slice(offset, offset + itemsPerPage);
+  const currentPageData = filteredList.slice(offset, offset + itemsPerPage);
+
   const renderCartItem = () => {
     return currentPageData.map((item, i) => {
-      return (
-        <CardItem
-          item={item}
-          key={i}
-        />
-      );
+      return <CardItem item={item} key={i} />;
     });
   };
 
   return (
     <div className='my-2'>
       <div className='grid grid-cols-12 gap-5 '>{renderCartItem()}</div>
-      <div className='flex justify-center'>
-        <ReactPaginate
-          breakLabel='...'
-          className='flex justify-center items-center gap-3 my-6'
-          nextLabel={
-            <span className='w-10 h-10 flex items-center justify-center bg-white rounded-md border border-solid'>
-              <GrNext />
-            </span>
-          }
-          pageRangeDisplayed={4}
-          pageCount={Math.ceil(products.length / itemsPerPage)}
-          previousLabel={
-            <span className='w-10 h-10 flex items-center justify-center bg-white rounded-md border border-solid'>
-              <GrPrevious />
-            </span>
-          }
-          pageClassName='border border-solid rounded-md py-2 px-4 hover:bg-main-red hover:text-white cursor-pointer'
-          activeClassName='bg-main-red text-white'
-          onPageChange={handlePageChange}
-        />
-      </div>
+      {pageCount > 1 && (
+        <div className='flex justify-center'>
+          <ReactPaginate
+            breakLabel='...'
+            className='flex justify-center items-center gap-3 my-6'
+            nextLabel={
+              <span className='w-10 h-10 flex items-center justify-center bg-white rounded-md border border-solid'>
+                <GrNext />
+              </span>
+            }
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            previousLabel={
+              <span className='w-10 h-10 flex items-center justify-center bg-white rounded-md border border-solid'>
+                <GrPrevious />
+              </span>
+            }
+            pageClassName='border border-solid rounded-md py-2 px-4 hover:bg-main-red hover:text-white cursor-pointer'
+            activeClassName='bg-main-red text-white'
+            onPageChange={handlePageChange}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -8,136 +8,14 @@ import FilterMoney from '../../components/client/FilterMoney';
 import ListProduct from '../../components/client/ListProduct';
 import requestHandle from '../../utils/requestHandle';
 
-const data = [
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/vn-galaxy-watch5-pro-den-4198d8e7-d2a8-4f73-9ea2-4ffebfeb6533.jpg?v=1672474572980',
-    price: '4.900.000',
-  },
-  {
-    name: 'B dong ho .',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/vn-galaxy-watch5-pro-den-4198d8e7-d2a8-4f73-9ea2-4ffebfeb6533.jpg?v=1672474572980',
-    price: '4.900.000',
-  },
-  {
-    name: 'dong ho sit .',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/vn-galaxy-watch5-pro-den-4198d8e7-d2a8-4f73-9ea2-4ffebfeb6533.jpg?v=1672474572980',
-    price: '4.900.000',
-  },
-  {
-    name: 'a dong ho .',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-gtr42.jpg?v=1666887735127',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-amazfit-gts-4den-1.jpg?v=1667843229057',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-amazfit-gts-4den-1.jpg?v=1667843229057',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-amazfit-gts-4den-1.jpg?v=1667843229057',
-    price: '4.900.000',
-  },
-  {
-    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore odit est debitis at cum, eveniet tenetur quod suscipit ipsam ratione dolorem nemo labore tempora. Cumque sint eligendi recusandae consectetur.',
-    discount: '10%',
-    img: 'https://bizweb.dktcdn.net/100/021/944/products/techwearvn-amazfit-gts-4den-1.jpg?v=1667843229057',
-    price: '4.900.000',
-  },
-];
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
     fetchProduct();
   }, []);
-
   const fetchProduct = async () => {
     try {
       const response = await requestHandle.get('product/');
@@ -148,6 +26,13 @@ export default function Home() {
       navigate('/error');
     }
   };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredList = products.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className='w-container m-auto mt-2'>
@@ -155,11 +40,15 @@ export default function Home() {
       <Brands />
       <Top5Product />
       <div className='relative z-30 flex justify-center py-10 gap-4'>
-        <Search data={data} />
+        <Search
+          products={products}
+          searchTerm={searchTerm}
+          handleSearch={handleSearch}
+        />
         <FilterMoney />
       </div>
       <div className='relative z-10'>
-        <ListProduct data={data} />
+        <ListProduct products={products} filteredList={filteredList}  />
       </div>
     </div>
   );
