725fcf60aa85ac7ea3dfdafe94c25d2268126abd
AnhTuan <voanhtuan13321@gmail.com>
2023-10-30 22:37:11 +0700

    done

--- src/components/admin/Siderbar.jsx
+++ src/components/admin/Siderbar.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { redirect, useNavigate } from 'react-router-dom';
+
+const navs = [
+  {
+    to: '/admin/brands',
+    text: 'Brands',
+  },
+  {
+    to: '/admin/products',
+    text: 'Products',
+  },
+  {
+    to: '/admin/orders',
+    text: 'Orders',
+  },
+];
 
 export default function Siderbar() {
+  const navigate = useNavigate();
+
+  const handleClickLogout = () => {
+    alert('Logout');
+  };
+
+  const renderNav = (nav) => {
+    return (
+      <div
+        key={nav.to}
+        onClick={() => navigate(nav.to)}
+        className='cursor-pointer hover:text-red-500 transition-all duration-150 ease-linear p-2 text-2xl'
+      >
+        {nav.text}
+      </div>
+    );
+  };
+
   return (
     <div className='h-screen shadow-2xl'>
       <h1 className='font-bold text-6xl text-center py-5'>logo</h1>
-      <div>siderbar</div>
+      <div className='p-10'>{navs.map((nav) => renderNav(nav))}</div>
+      <div className='text-center'>
+        <button
+          className='bg-red-600 text-white text-lg px-5 py-3 rounded-lg hover:bg-red-500 transition-all duration-150 ease-linear'
+          onClick={handleClickLogout}
+        >
+          LOGOUT
+        </button>
+      </div>
     </div>
   );
 }
